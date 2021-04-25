0944e08a3d86b6086a83a58d410baff8ba70743e
lucas <lucas@LAPTOP-431QTJM1>
2021-04-25 15:15:56 -0300

    q cara bom

--- ProjetoEclipse/src/negocios/Teste.java
+++ ProjetoEclipse/src/negocios/Teste.java
@@ -3,7 +3,7 @@ package negocios;
 public class Teste {
 	
 	public static void main(String[] args) {
-		System.out.println("Teste Dois YAGO");
+		System.out.println("Teste Dois Leno mamão");
 	}
 
 }
