e8e83e54a28bd6ff0a5a65db9ff886571b932fa0
Lenon <razorraver1@hotmail.com>
2021-04-25 15:14:34 -0300

    Commit.

--- ProjetoEclipse/src/negocios/Teste.java
+++ ProjetoEclipse/src/negocios/Teste.java
@@ -3,7 +3,7 @@ package negocios;
 public class Teste {
 	
 	public static void main(String[] args) {
-		System.out.println("Teste Dois");
+		System.out.println("Teste Dois Teste Dois");
+		System.out.println("Eclipse");
 	}
-
 }
