84b7d6da3cbc6cb0422e3019b2c67358d31f315c
JocaMotta <83181624+JocaMotta@users.noreply.github.com>
2021-06-06 00:48:25 -0300

    Add files via upload

---
+++ ProjetoEclipse/src/negocios/Pessoa.java
@@ -0,0 +1,73 @@
+public abstract class Pessoa {
+	
+	private String nome;
+	private String cpf;
+	
+	public Pessoa(String nome, String cpf) {
+		
+		setNome(nome);
+		setCpf(cpf);
+		
+	}
+	
+	public String getNome() {
+		
+		return nome;
+		
+	}
+	
+	public void setNome(String nome) {
+		
+		if (nome != null) {
+			
+			this.nome = nome;
+			
+		}
+		
+	}
+	
+	public String getCpf() {
+		
+		return cpf;
+		
+	}
+	
+	public void setCpf(String cpf) {
+		
+		if (cpf != null) {
+			
+			this.cpf = cpf;
+			
+		}
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		boolean resultado = false;
+		
+		if (obj instanceof Pessoa) {
+			
+			Pessoa p = (Pessoa) obj;
+			
+			if (p.nome != null && p.cpf != null && this.nome.equalsIgnoreCase(p.nome) && this.cpf.equalsIgnoreCase(p.cpf)) {
+				
+				resultado = true;
+				
+			}
+			
+		}
+		
+		return resultado;
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "\nNome: " + nome + " | CPF: " + cpf;
+		
+	}
+	
+}
