9cab8fe6de08ed26c99b72a45856ae5a066524f5
Lenon <razorraver1@hotmail.com>
2021-06-06 00:11:19 -0300

    Commit de desbugar

--- ProjetoEclipse/src/negocios/Teste.java
+++ ProjetoEclipse/src/negocios/Teste.java
@@ -6,16 +6,6 @@ public class Teste {
 
 		System.out.println("Teste Dois TESTANDO");
 		//caraio q saco
-		//1 comentario 
-		// foi ja essa desgraça aq chaguibas
-		//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-		//pull Yago
-		//pull Joca Cacetudo rei de todas, vo dormir flw
-		//mentira, to acordado ainda fml, agr eh a vera, flw
-		//TESTANDO, deem pull.
-		// DANDO PULL, (ATT JOCA CACETUDO 2021)
-		// DNV
-		// olha eu aqui dnv
 		
 	}
 }
