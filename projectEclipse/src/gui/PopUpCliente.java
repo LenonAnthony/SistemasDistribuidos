4e505f6ec4f9672984179560843d429579afc727
Lenon <razorraver1@hotmail.com>
2021-06-22 00:50:45 -0300

    Atualizando PopUps

--- projectEclipse/src/gui/PopUpCliente.java
+++ projectEclipse/src/gui/PopUpCliente.java
@@ -7,19 +7,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import negocios.ControladorCliente;
 import negocios.ControladorFuncionario;
+import negocios.basicos.Cliente;
 
 import java.awt.SystemColor;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PopUpCliente extends JFrame {
+
+	public static ControladorCliente cc = new ControladorCliente();
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
@@ -52,18 +59,18 @@ public class PopUpCliente extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(SystemColor.info);
 		panel.setBounds(10, 11, 414, 186);
 		contentPane.add(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{49, 31, 328, 0};
-		gbl_panel.rowHeights = new int[]{31, 20, 31, 20, 31, 20, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 49, 31, 328, 0 };
+		gbl_panel.rowHeights = new int[] { 31, 20, 31, 20, 31, 20, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JLabel lblNewLabel = new JLabel("Nome:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
@@ -71,7 +78,7 @@ public class PopUpCliente extends JFrame {
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 1;
 		panel.add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
@@ -81,7 +88,7 @@ public class PopUpCliente extends JFrame {
 		gbc_textField.gridy = 1;
 		panel.add(textField, gbc_textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("CPF:");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.fill = GridBagConstraints.HORIZONTAL;
@@ -89,7 +96,7 @@ public class PopUpCliente extends JFrame {
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 3;
 		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
@@ -99,7 +106,7 @@ public class PopUpCliente extends JFrame {
 		gbc_textField_1.gridy = 3;
 		panel.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Endere\u00E7o:");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
@@ -107,7 +114,7 @@ public class PopUpCliente extends JFrame {
 		gbc_lblNewLabel_2.gridx = 0;
 		gbc_lblNewLabel_2.gridy = 5;
 		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
+
 		textField_2 = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
@@ -116,8 +123,23 @@ public class PopUpCliente extends JFrame {
 		gbc_textField_2.gridy = 5;
 		panel.add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Confirmar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				if (textField.getText().isEmpty() || textField_1.getText().isEmpty()
+						|| textField_2.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Favor, preencher tudo.");
+				}
+				else {
+					Cliente c = new Cliente(textField.getText(), textField_1.getText(), textField_2.getText(), 0);
+					cc.cadastrar(c);
+					System.out.println(cc);
+				}
+
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		btnNewButton.setBounds(126, 208, 158, 42);
 		contentPane.add(btnNewButton);
--- projectEclipse/src/gui/PopUpProduto.java
+++ projectEclipse/src/gui/PopUpProduto.java
@@ -6,17 +6,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import negocios.ControladorProduto;
+import negocios.basicos.Produto;
+
 import java.awt.SystemColor;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PopUpProduto extends JFrame {
 
+	public static ControladorProduto cp = new ControladorProduto();
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
@@ -49,25 +58,25 @@ public class PopUpProduto extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(SystemColor.info);
 		panel.setBounds(10, 11, 414, 167);
 		contentPane.add(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 0, 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JLabel lblNewLabel = new JLabel("Nome:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		panel.add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 0);
@@ -76,14 +85,14 @@ public class PopUpProduto extends JFrame {
 		gbc_textField.gridy = 0;
 		panel.add(textField, gbc_textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Pre\u00E7o:");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 2;
 		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
@@ -92,14 +101,14 @@ public class PopUpProduto extends JFrame {
 		gbc_textField_1.gridy = 2;
 		panel.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Descri\u00E7\u00E3o:");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
 		gbc_lblNewLabel_2.gridx = 0;
 		gbc_lblNewLabel_2.gridy = 4;
 		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
+
 		textField_2 = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
@@ -107,8 +116,23 @@ public class PopUpProduto extends JFrame {
 		gbc_textField_2.gridy = 4;
 		panel.add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Confirmar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().isEmpty() || textField_1.getText().isEmpty()
+						|| textField_2.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Favor, preencher tudo.");
+				} else {
+					Produto p = new Produto(textField.getText(), textField_2.getText(), 1,
+							Double.parseDouble(textField_1.getText()), true);
+					cp.cadastrar(p);
+					System.out.println(cp);
+
+				}
+
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		btnNewButton.setBounds(111, 193, 207, 57);
 		contentPane.add(btnNewButton);
